afb13a32c0928953abef4810edd88e6d36b7f439
Valentin Raduti <valentin@thedotin.ro>
2018-07-17 20:13:52 +0300

    Initial

---
+++ src/main/java/com/viavisolutions/candlesticks/CandlesticksApplication.java
@@ -0,0 +1,12 @@
+package com.viavisolutions.candlesticks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandlesticksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandlesticksApplication.class, args);
+	}
+}
